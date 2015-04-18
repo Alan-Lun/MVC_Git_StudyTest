4abe2c2354a942847580fb20ea9072ec459294c7
Alan-Lun <s52076663135@gmail.com>
2015-04-18 16:11:45 +0800

    裏頭有加上EF及20150418今天上課的到model部分

---
+++ MVC_study/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_study.Startup))]
+namespace MVC_study
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
